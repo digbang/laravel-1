6fec2711c7d79c6bc76dbcfe0cb7cd0e8686d813
Guido Contreras Woda <guiwoda@gmail.com>
2016-09-26 11:06:29 -0300

    Moved closure commands to the Console Kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -31,6 +31,10 @@ class Kernel extends ConsoleKernel
     /**
      * Register the commands for the application.
      *
+     * This is where you may define all of your Closure based console
+     * commands. Each Closure is bound to a command instance allowing a
+     * simple approach to interacting with each command's IO methods.
+     *
      * @return void
      */
     protected function commands()
